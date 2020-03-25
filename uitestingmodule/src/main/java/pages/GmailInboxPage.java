405904b5a1df0adbedeb02580469ccbd693bf4a1
Diana Dmitriienko <diana_dmitriienko@epam.com>
2020-03-25 19:15:06 +0200

    add xpathes for writing email dialog

--- uitestingmodule/src/main/java/pages/GmailInboxPage.java
+++ uitestingmodule/src/main/java/pages/GmailInboxPage.java
@@ -1,9 +1,9 @@
 package pages;
 
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,15 +17,49 @@ public class GmailInboxPage extends PageObject {
     WebElementFacade loggedInUserIcon;
     @FindBy(xpath = "//div/a[contains(@href,'Logout')]")
     WebElementFacade logoutButton;
+    //write letter dialog
+    @FindBy(xpath = "//textarea[@role='combobox'][1]")
+    WebElementFacade toWhomInput;
+    @FindBy(xpath = "//textarea[@role='combobox'][2]")
+    WebElementFacade toWhomCopyInput;
+    @FindBy(xpath = "//input[@name='subjectbox']")
+    WebElementFacade letterSubjectInput;
+    @FindBy(xpath = "//div[@role='textbox']")
+    WebElementFacade letterBodyInput;
+    @FindBy (xpath = "//div[@role='dialog']/descendant::img[3]")
+    WebElementFacade closeLetterDialogIcon;
     private String iconXpath = "//div/a[contains(@aria-label,'%s')]";
 
-    public WebElementFacade getEmailInput() {
-        return buttonWriteEmail;
+    public WebElementFacade getToWhomCopyInput() {
+        return toWhomCopyInput;
+    }
+    public WebElementFacade getCloseLetterDialogIcon() {
+        return closeLetterDialogIcon;
     }
 
     public void waitPageToLoad() {
         new WebDriverWait(getDriver(), 10, 20)
                 .until(ExpectedConditions.visibilityOf(getEmailInput()));
+    }
+
+    public WebElementFacade getButtonWriteEmail() {
+        return buttonWriteEmail;
+    }
+
+    public WebElementFacade getToWhomInput() {
+        return toWhomInput;
+    }
+
+    public WebElementFacade getLetterSubjectInput() {
+        return letterSubjectInput;
+    }
+
+    public WebElementFacade getLetterBodyInput() {
+        return letterBodyInput;
+    }
+
+    public WebElementFacade getEmailInput() {
+        return buttonWriteEmail;
     }
 
     public WebElementFacade getLoggedInUserIcon(String email) {
